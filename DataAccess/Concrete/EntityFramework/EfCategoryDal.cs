4dd8b17d8575c0697244ce7a3990d42d43791daf
hamzaazman <hamzaazman6776@gmail.com>
2021-02-03 22:03:02 +0300

    EfCategoryDal Added EfProductDal Added Customer Added

--- DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -7,29 +7,29 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfCategoryDal : ICategoryDal
     {
-        public void Add(Product entity)
+        public void Add(Category entity)
         {
             throw new NotImplementedException();
         }
 
-        public void Delete(Product entity)
+        public void Delete(Category entity)
         {
             throw new NotImplementedException();
         }
 
-        public Product Get(Expression<Func<Product, bool>> filter)
+        public Category Get(Expression<Func<Category, bool>> filter)
         {
             throw new NotImplementedException();
         }
 
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
         {
             throw new NotImplementedException();
         }
 
-        public void Update(Product entity)
+        public void Update(Category entity)
         {
             throw new NotImplementedException();
         }
